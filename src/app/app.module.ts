46e67edaea544bd740ae204dc1df55f5de458c7c
AK <kasssalex@gmail.com>
2018-01-28 23:09:06 +0100

    Include all external modules we need into main application module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { Ng2AutoCompleteModule } from 'ng2-auto-complete';
+import { TranslateModule, TranslateLoader, TranslateService  } from '@ngx-translate/core'
+
+
 
 
 @NgModule({
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, ReactiveFormsModule,
+    HttpClientModule,
+    Ng2AutoCompleteModule,
+    NgbModule.forRoot(),
+    TranslateModule.forRoot(),
+    RouterModule.forRoot([
+    ])    
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
