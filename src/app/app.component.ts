2f5a59553bb44229492232c0c6fec48449226fd5
AK <kasssalex@gmail.com>
2018-04-20 19:11:28 +0200

    Pass the AppID to server

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx'
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import { WebSocketService, ConfigService } from 'gg-basic-data-services'
+import { DataStore, WebSocketService, ConfigService } from 'gg-basic-data-services'
 import { TranslateService } from '@ngx-translate/core';
 import { environment } from '../environments/environment';
 
@@ -11,10 +11,12 @@ import { environment } from '../environments/environment';
     templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-    constructor(private webSocketService: WebSocketService, public translate: TranslateService, private configService: ConfigService ) {
+    constructor(private webSocketService: WebSocketService, public translate: TranslateService, private configService: ConfigService, private dataStore: DataStore ) {
         
         this.configService.setProduction(environment.production)
-        this.webSocketService.init()
+        this.dataStore.setApplication(this.dataStore.XENIA)         // ici ce ne sera pas XENIA, mais un id different
+        this.webSocketService.init(this.dataStore.XENIA)    //
+        
        
         setTimeout(() => {
             this.webSocketService.requeryDb()
